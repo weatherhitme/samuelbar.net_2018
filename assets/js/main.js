f9331c52e212cd2e222d8a169f6ea520aa83dc82
Sam B <sam_barnettxx@hotmail.com>
2018-10-04 21:38:28 +0800

    Finished CTA & started services

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$('.hero-container').slick({
+    arrows: false,
+    cssEase: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    draggable: false,
+    swipe: false,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1,
+})
+
+$(document).ready(function () {
+
+});
