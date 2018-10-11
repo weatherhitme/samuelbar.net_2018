9b0fec30372e45dc807c257581452ee0349c2200
Sam B <sam_barnettxx@hotmail.com>
2018-10-12 00:33:30 +0800

    Vert/Horiz Hero & Tab-only focus

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,36 @@ $('.hero-container').slick({
     infinite: true,
     speed: 600,
     slidesToShow: 1,
+    vertical: true,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            vertical: false,
+        }
+    }]
 })
 
 $(document).ready(function () {
-
+    $('.loading-hide').removeClass('loading-hide');
+    tabbingBehaviour();
 });
+
+function tabbingBehaviour() {
+
+    function handleFirstTab(e) {
+        if (e.keyCode === 9) {
+            document.body.classList.add('user-is-tabbing');
+
+            window.removeEventListener('keydown', handleFirstTab);
+            window.addEventListener('mousedown', handleMouseDownOnce);
+        }
+    }
+    function handleMouseDownOnce() {
+        document.body.classList.remove('user-is-tabbing');
+
+        window.removeEventListener('mousedown', handleMouseDownOnce);
+        window.addEventListener('keydown', handleFirstTab);
+    }
+
+    window.addEventListener('keydown', handleFirstTab);
+}
